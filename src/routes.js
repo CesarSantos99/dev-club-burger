fcf80f18ab0c0a1051fdd9e242ee05a67aad33ab
Cesar Santos <cesarsantossouza99@gmail.com>
2022-12-28 19:58:54 -0300

    andamento

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-  return response.json({ menssage: 'Hello Word' })
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Cesar',
+    email: 'cesar@gmail.com',
+    password_hash: '5252'
+  })
+  return response.json(user)
 })
 
 export default routes
